9cb02c0a2b02b36fe5f08db9565af97e3377d0e7
sr-shohan <srshohan026@gmail.com>
2020-10-15 23:56:54 +0600

    initial setup and and update

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+
+
+const home = () => {
+  return (
+    <>
+      <Hero/>
+        
+     
+     
+    </>
+  );
+};
+
+export default home;
